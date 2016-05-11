ccba0621c5e4b5340819c50596ec3595aa05517b
John May <john@nextmovesoftware.com>
2016-05-11 22:21:35 +0100

    Correction abbreviation dictionary.

--- src/main/java/org/openscience/cdk/app/DepictController.java
+++ src/main/java/org/openscience/cdk/app/DepictController.java
@@ -84,7 +84,7 @@ public class DepictController {
 
     public DepictController() throws IOException {
         int count = this.abbreviations.loadFromFile("/org/openscience/cdk/app/abbreviations.smi");
-        this.reagents.loadFromFile("/org/openscience/cdk/app/agents.smi");
+        this.reagents.loadFromFile("/org/openscience/cdk/app/reagents.smi");
     }
 
     /**
