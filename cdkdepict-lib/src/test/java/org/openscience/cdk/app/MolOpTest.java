78e08c169cdf14d0d6ff56e4059c31aacc2a3a52
John Mayfield <john@nextmovesoftware.com>
2020-01-01 18:00:16 +0000

    Add test case

--- cdkdepict-lib/src/test/java/org/openscience/cdk/app/MolOpTest.java
+++ cdkdepict-lib/src/test/java/org/openscience/cdk/app/MolOpTest.java
@@ -9,6 +9,7 @@ package org.openscience.cdk.app;
 import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
@@ -21,6 +22,18 @@ public class MolOpTest {
   @Test public void NO2() throws InvalidSmilesException {
     SmilesParser   smipar = new SmilesParser(SilentChemObjectBuilder.getInstance());
     IAtomContainer mol    = smipar.parseSmiles("[Co][N+]([O-])(=O)");
+    MolOp.perceiveDativeBonds(mol);
+    int count = 0;
+    for (IBond bond : mol.bonds()) {
+      if (bond.getDisplay() != IBond.Display.Solid)
+        count++;
+    }
+    assertThat(count, CoreMatchers.is(1));
+  }
+
+  @Test public void NO3() throws InvalidSmilesException {
+    SmilesParser   smipar = new SmilesParser(SilentChemObjectBuilder.getInstance());
+    IAtomContainer mol    = smipar.parseSmiles("[O-][N+]([O-])=O");
     MolOp.perceiveDativeBonds(mol);
     int count = 0;
     for (IBond bond : mol.bonds()) {
