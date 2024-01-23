d1dbad005f7a544e8d5b9aaf0057190168a2060c
John Mayfield <john@nextmovesoftware.com>
2024-01-23 20:06:17 +0000

    More hydrogen options

--- cdkdepict-lib/src/main/java/org/openscience/cdk/app/HydrogenDisplayType.java
+++ cdkdepict-lib/src/main/java/org/openscience/cdk/app/HydrogenDisplayType.java
@@ -4,9 +4,37 @@
 
 package org.openscience.cdk.app;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 enum HydrogenDisplayType {
-  Provided,
-  Minimal,
-  Stereo,
-  Smart;
+  Provided('P'),
+  Minimal('M'),
+  Stereo('C'),
+  Smart('S'),
+  Explicit('X');
+
+  private final char ch;
+  private static Map<String, HydrogenDisplayType> map = new HashMap<>();
+
+  static {
+    for (HydrogenDisplayType hdisp : values()) {
+      map.put(hdisp.name(), hdisp);
+      map.put(hdisp.name().toLowerCase(Locale.ROOT), hdisp);
+      map.put(Character.toString(hdisp.ch), hdisp);
+    }
+    map.put("suppressed", HydrogenDisplayType.Minimal);
+    map.put("bridgeheadtetrahedral", HydrogenDisplayType.Smart);
+    map.put("bridgehead", HydrogenDisplayType.Smart);
+    map.put("default", HydrogenDisplayType.Smart);
+  }
+
+  HydrogenDisplayType(char ch) {
+    this.ch = ch;
+  }
+
+  static HydrogenDisplayType parse(String key) {
+    return map.getOrDefault(key, HydrogenDisplayType.Smart);
+  }
 }
